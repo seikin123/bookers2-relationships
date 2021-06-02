59626e12e90147f8a3faf0ea060cab8bcc390842
ヨシダタイキ <abc521fjr7@gmail.com>
2021-06-02 16:52:16 +0900

    アソシエーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, length: { in: 1..200 }
   belongs_to :user
+
+  # フォロー機能
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :books, dependent: :destroy
+
+  # フォロー機能
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+
 end
